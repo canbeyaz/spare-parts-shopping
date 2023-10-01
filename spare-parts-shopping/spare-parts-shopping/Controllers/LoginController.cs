46123641831708b9b558cac09a3f7cc01026e7b5
canbeyaz <hasancanbeyaz@hotmail.com>
2023-10-02 01:08:47 +0300

    initial commit

---
+++ spare-parts-shopping/spare-parts-shopping/Controllers/LoginController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace spare_parts_shopping.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        [Route("Login")]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [Route("Signup")]
+        public ActionResult Signup()
+        {
+            return View();
+        }
+    }
+}
